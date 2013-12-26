3a417302b23273a5836c9733eabd25fbf6a00c4a
nick <nick@linux-s865.site>
2013-12-26 16:18:03 -0600

    cd init impl

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,28 @@ use std::io::buffered::BufferedReader;
 use std::io;
 use std::run::Process;
 use std::run::ProcessOptions;
+use std::run::ProcessOutput;
+use std::path::posix::Path;
 
 fn get_working_dir() -> ~str {
     let elts: ~[(~str,~str)] = env();
-    let mut working_dir = ~"";
     
     for elt in elts.iter() {
         let (ref s1, ref s2): (~str,~str) = *elt;
         if *s1 == ~"PWD" {
-            working_dir = s2.clone();
-        }
-    }
-    working_dir
-}
-
-fn get_path() -> ~str {
-    let elts: ~[(~str,~str)] = env();
-    for elt in elts.iter() {
-        let (ref s1, ref s2): (~str, ~str) = *elt;
-        if *s1 == ~"PATH" {
             return s2.clone()
         }
     }
-    ~""
+    return ~""
+}
+
+fn set_working_dir(d: &str) {
+    let mut new_pwd: ~str = get_working_dir();
+    let old_pwd: &str = new_pwd;
+
+    
+    std::os::setenv(&"PWD", d);
+    std::os::setenv(&"OLDPWD", old_pwd);
 }
 
 fn create_cmd(cmd: &str) -> ~[~str]{
@@ -49,18 +48,41 @@ fn create_process(s: &str) -> Option<Process> {
     if line.len() > 1 {
         args = line.slice(1, line.len());
     }
+    
+    if cmd == "cd" {
+        let mut cd_dir: Path = Path::new("~/");
+        if args.len() > 0 {
+            cd_dir = Path::new(args[0].clone());
+        }
+        let ret_val: bool = std::os::change_dir(&cd_dir);
+        if ret_val == false {
+            println(format!("Error: '{}' is not a valid path.", args[0]));
+        } else {
+            set_working_dir(args[0]);
+        }
+        return None
+    }
+
     let opts: ProcessOptions = ProcessOptions::new();
     let launch: Option<Process> = Process::new(cmd, args, opts);
     launch
 }
 
-fn handle_process(new_proc: &Option<Process>) {
-    match new_proc {
-        Some(p) => {
-            
-        }
-        None => println("None")
+fn handle_process(new_proc: ~Process) -> bool {
+    let mut new_proc = new_proc;
+    let proc_out: ProcessOutput = new_proc.finish_with_output();
+    let output: &str = std::str::from_utf8(proc_out.output);
+    let error: &str = std::str::from_utf8(proc_out.error);
+    if output.len() != 0 {
+        println(format!("{}", output));
     }
+    if error.len() != 0 {
+        println(format!("{}", error));
+    }
+    if !proc_out.status.success() {
+        return false
+    }
+    true
 }
 
 fn read_stdin() {
@@ -69,9 +91,12 @@ fn read_stdin() {
         let line = reader.read_line();
         match line {
             Some(ref s) => {
-                println("New line");
                 let new_proc: Option<Process> = create_process(*s);
-                handle_process(&new_proc);
+                if new_proc.is_some() {
+                    handle_process(~(new_proc.unwrap()));
+                }
+                print(format!("{}$> ", get_working_dir()));
+                std::io::stdio::flush();
             },
             None => println("None")
         }
@@ -79,8 +104,7 @@ fn read_stdin() {
 }
 
 fn main() {
-    let working_dir = get_working_dir();
-    print(format!("{}$ ", working_dir));
+    print(format!("{}$> ", get_working_dir()));
     std::io::stdio::flush();
     read_stdin();
 }
