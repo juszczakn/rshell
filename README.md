9fd05a81645070dc6d9911aa9a25d41ce9e7471a
nick <nick@linux-s865.site>
2013-12-26 19:50:19 -0600

    basic functionality done

---
+++ README.md
@@ -0,0 +1,29 @@
+rshell
+=========
+
+A basic shell written and tested with rust-0.9pre. Not even close to garunteed to compile with later versions, as rust is still changing very quickly.
+
+As my first program, this was just to get a feeling for the language, and I'm sure is very buggy.
+
+Compile
+=========
+
+No dependencies on anything outside of the stdlib, simply compile with
+
+```rustc main.rs```
+
+Usage
+=========
+
+Very basic. Allows for changing directories and calling executables found in your $PATH with parameters which only have output (not tested with executables that require input).
+
+Problems
+=========
+
+- Currently fails when an invalid command is given.
+- Does not support input
+
+License
+=========
+
+MIT
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,51 @@
 use std::os::env;
-use std::io::buffered::BufferedReader;
 use std::io;
-use std::run::Process;
-use std::run::ProcessOptions;
-use std::run::ProcessOutput;
-use std::path::posix::Path;
+use std::io::buffered::BufferedReader;
+use std::run::{Process, ProcessOptions, ProcessOutput};
 
-fn get_working_dir() -> ~str {
+/* Compiles and tested with rust-0.9pre.
+A very basic shell program which allows for changing directories */
+
+fn get_working_dir(old_dir: bool) -> ~str {
     let elts: ~[(~str,~str)] = env();
     
     for elt in elts.iter() {
         let (ref s1, ref s2): (~str,~str) = *elt;
-        if *s1 == ~"PWD" {
-            return s2.clone()
+        if old_dir {
+            if *s1 == ~"OLDPWD" {
+                return s2.clone()
+            }
+        } else {
+            if *s1 == ~"PWD" {
+                return s2.clone()
+            }
         }
     }
     return ~""
 }
 
 fn set_working_dir(d: &str) {
-    let mut new_pwd: ~str = get_working_dir();
-    let old_pwd: &str = new_pwd;
+    if d.len() == 0 {
+        return
+    }
+    let old_pwd: &str = get_working_dir(false);
+    let mut new_pwd: Path = Path::new(old_pwd);
 
-    
-    std::os::setenv(&"PWD", d);
-    std::os::setenv(&"OLDPWD", old_pwd);
+    // 45 == '-', go to old_pwd
+    if d[0] == 45 {
+        new_pwd = Path::new(get_working_dir(true));
+        // 47 == '/', absolute path
+    } else if d[0] == 47 {
+        new_pwd = Path::new(d);
+    } else {
+        new_pwd.push(d);
+    }
+
+    let n: Option<&str> = new_pwd.as_str();
+    if n.is_some() {
+        std::os::setenv(&"PWD", n.unwrap());
+        std::os::setenv(&"OLDPWD", old_pwd);
+    }
 }
 
 fn create_cmd(cmd: &str) -> ~[~str]{
@@ -50,15 +71,26 @@ fn create_process(s: &str) -> Option<Process> {
     }
     
     if cmd == "cd" {
-        let mut cd_dir: Path = Path::new("~/");
+        let mut cd_dir: Path = Path::new("/home/nick");
         if args.len() > 0 {
-            cd_dir = Path::new(args[0].clone());
+            let mut new_path = args[0].clone();
+            if new_path[0] == 45 {
+                new_path = get_working_dir(true);
+            }
+            cd_dir = Path::new(new_path);
         }
         let ret_val: bool = std::os::change_dir(&cd_dir);
+
+        let dir: Option<&str> = cd_dir.as_str();
         if ret_val == false {
-            println(format!("Error: '{}' is not a valid path.", args[0]));
+            if dir.is_some() {
+                println(format!("Error: '{}' not a valid path.",
+                                dir.unwrap()));
+            }
         } else {
-            set_working_dir(args[0]);
+            if dir.is_some() {
+                set_working_dir(dir.unwrap());
+            }
         }
         return None
     }
@@ -89,22 +121,23 @@ fn read_stdin() {
     let mut reader = BufferedReader::new(io::stdin());
     while !reader.eof() {
         let line = reader.read_line();
-        match line {
-            Some(ref s) => {
-                let new_proc: Option<Process> = create_process(*s);
-                if new_proc.is_some() {
-                    handle_process(~(new_proc.unwrap()));
-                }
-                print(format!("{}$> ", get_working_dir()));
-                std::io::stdio::flush();
-            },
-            None => println("None")
+        if line.is_some(){
+            let line: ~str = line.unwrap();
+            if line == ~"exit\n" {
+                return
+            }
+            let new_proc: Option<Process> = create_process(line);
+            if new_proc.is_some() {
+                handle_process(~(new_proc.unwrap()));
+            }
+            print(format!("{}$> ", get_working_dir(false)));
+            std::io::stdio::flush();
         }
     }
 }
 
 fn main() {
-    print(format!("{}$> ", get_working_dir()));
+    print(format!("{}$> ", get_working_dir(false)));
     std::io::stdio::flush();
     read_stdin();
 }
